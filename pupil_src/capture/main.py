e911fbc8389babf8c134ab8abd8b5d9d388e5733
Sheharyar Naseer <sheharyar92@live.com>
2014-03-26 07:19:17 +0500

    Camera Settings updated

--- pupil_src/capture/main.py
+++ pupil_src/capture/main.py
@@ -92,12 +92,12 @@ else:
 
 def main():
     # To assign camera by name: put string(s) in list
-    eye_src = ["Microsoft", "6000","Integrated Camera"]
-    world_src = ["Logitech Camera","UVC camera","C510","B525", "C525","C615","C920","C930e"]
+    #eye_src = ["Microsoft", "6000","Integrated Camera"]
+    #world_src = ["Logitech Camera","UVC camera","C510","B525", "C525","C615","C920","C930e"]
 
     # to assign cameras directly, using integers as demonstrated below
-    # eye_src = 1
-    # world_src = 0
+    eye_src = 1
+    world_src = 0
 
     # to use a pre-recorded video.
     # Use a string to specify the path to your video file as demonstrated below
